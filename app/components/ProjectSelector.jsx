9614c82144443cea243bf2881462c534a8e4f37e
David Messerer <messerer.da@gmail.com>
2017-06-02 14:30:30 +0200

    ProjectSelector Component --> added click and cond rendering of div

--- app/components/ProjectSelector.jsx
+++ app/components/ProjectSelector.jsx
@@ -16,16 +16,72 @@ const styles = {
   icon: {
     float: 'right',
   },
+  drop: {
+    position: 'outside',
+    backgroundColor: Theme.colors.WHITE,
+    width: '100%',
+    height: `calc(100% - ${Theme.sizes.SYSTEM_BAR_BOTTOM_HEIGHT}px)`,
+    fontFamily: Theme.fonts.MAIN_FONT_FAMILY,
+    color: Theme.colors.WHITE,
+  },
+  dropIn: {
+    position: 'absolute',
+    backgroundColor: Theme.colors.WHITE,
+    width: '100%',
+    height: `calc(100% - ${Theme.sizes.SYSTEM_BAR_BOTTOM_HEIGHT}px)`,
+    fontFamily: Theme.fonts.MAIN_FONT_FAMILY,
+    color: Theme.colors.WHITE,
+  },
+};
+
+const dTest = {
+  button: {
+    position: 'absolute',
+    top: '16px',
+    right: '16px',
+    display: 'block',
+    marginBottom: '16px',
+    backgroundColor: Theme.colors.BACKGROUND,
+    color: Theme.colors.FONT_DEFAULT,
+    fontWeight: '200',
+    outline: false,
+    cursor: 'pointer',
+  },
 };
 
 class ProjectSelector extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      fadeIn: false,
+    };
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
+
+  onButtonClick() {
+    if (!this.state.fadeIn) {
+      this.setState({
+        fadeIn: true,
+      });
+    } else {
+      this.setState({
+        fadeIn: false,
+      });
+    }
+  }
+
+
   render() {
     return <div style={styles.container}>
-      <div><img src="" /></div>
-      <div>Select Project <i className="material-icons" style={styles.icon}>arrow_drop_down</i></div>
-    </div>;
+          <div><img src="" /></div>
+          <div>Select Project <button onClick={this.onButtonClick} style={dTest.button}> <i className="material-icons" style={styles.icon}>arrow_drop_down</i></button></div>
+        {this.state.fadeIn ? <div style={styles.dropIn}></div> : null}
+        {!this.state.fadeIn ? <div style ={styles.drop}></div> : null}
+        </div>;
   }
+
+
 }
 
 export default ProjectSelector;
