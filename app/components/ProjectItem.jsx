38340744fcb06e7002ed37517c84c131f99eefae
David Messerer <messerer.da@gmail.com>
2017-06-10 08:56:08 +0200

    Added ProjItem

---
+++ app/components/ProjectItem.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Theme from './../theme';
+
+const styles = {
+  container: {
+    width: '100%',
+    height: `${Theme.sizes.MENU_ITEM_HEIGHT}px`,
+    float: 'left',
+    padding: '11px',
+    marginBottom: '15px',
+  },
+  icon: {
+    fontSize: '30px',
+    color: Theme.colors.EDON_BLUE_LIGHT,
+  },
+};
+
+class ProjectItem extends React.Component {
+
+  static propTypes = {
+    icon: PropTypes.string,
+  };
+
+  render() {
+    return <div style={styles.container}>
+            <div><i className="material-icons" style={styles.icon}>{this.props.icon}</i></div>
+        </div>;
+  }
+}
+
+export default ProjectItem;
